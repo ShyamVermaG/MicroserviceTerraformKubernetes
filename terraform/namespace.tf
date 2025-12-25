a9aefba38b74104bc02fdaab8796faa2dca26ed3
ShyamVermaG <sv469370@gmail.com>
2025-12-25 21:41:09 +0530

    sdf

--- terraform/namespace.tf
+++ terraform/namespace.tf
@@ -3,8 +3,3 @@ resource "kubernetes_namespace" "microservices" {
     name = "microservices"
   }
 }
-resource "kubernetes_namespace_v1" "microservices" {
-  metadata {
-    name = "microservices"
-  }
-}
