098722649307d0740192a13aa45be0796442ec63
ShyamVermaG <sv469370@gmail.com>
2025-12-26 11:37:45 +0530

    dafa

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,18 @@ variable "region" {
 variable "zone" {
   default = "asia-south1-a"
 }
+variable "kube_host" {
+  type        = string
+  description = "Kubernetes API server endpoint"
+}
+
+variable "kube_token" {
+  type        = string
+  description = "Service account token"
+  sensitive   = true
+}
+
+variable "kube_ca" {
+  type        = string
+  description = "Base64 encoded cluster CA cert"
+}
